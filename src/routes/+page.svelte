b490e793d68d1fc8ed16d0f9ad782debd408e8bb
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-12-22 11:53:02 -0800

    Add plus button to bottom right

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<p>hi</p>
+<div class="fixed bottom-8 right-8">
+	<button
+		class="flex h-16 w-16 items-center justify-center rounded-xl bg-blue-800 text-4xl hover:bg-blue-700"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="h-6 w-6 stroke-white"
+		>
+			<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+		</svg>
+	</button>
+</div>
