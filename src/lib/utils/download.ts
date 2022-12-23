c0ce41b57866180c957085a83ad3c0afce72d46c
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-12-23 10:47:30 -0800

    Simplify variable names

--- src/lib/utils/download.ts
+++ src/lib/utils/download.ts
@@ -13,22 +13,22 @@ export async function downloadJar(version: string, path: string) {
 async function getDownloadUrl(version: string) {
 	const client = await getClient();
 
-	const paperVersionsList = await (
+	const versionsList = await (
 		await client.get<PaperVersionsList>('https://papermc.io/api/v2/projects/paper')
 	).data;
 
 	// we can assume that the version exists because the ui only shows versions that exist
-	const paperVersion = paperVersionsList.versions.find((ver) => ver === version) as string;
+	const paperVersion = versionsList.versions.find((ver) => ver === version) as string;
 
 	// gets list of builds for the specified version
-	const paperBuildsList = await (
+	const buildsList = await (
 		await client.get<PaperBuildsList>(
 			`https://papermc.io/api/v2/projects/paper/versions/${paperVersion}`
 		)
 	).data;
 
 	// gets the last item in order to get the most recent build
-	const paperBuild = paperBuildsList.builds[paperBuildsList.builds.length - 1];
+	const latestBuild = buildsList.builds[buildsList.builds.length - 1];
 
-	return `https://papermc.io/api/v2/projects/paper/versions/${paperVersion}/builds/${paperBuild}/downloads/paper-${paperVersion}-${paperBuild}.jar`;
+	return `https://papermc.io/api/v2/projects/paper/versions/${paperVersion}/builds/${latestBuild}/downloads/paper-${paperVersion}-${latestBuild}.jar`;
 }
