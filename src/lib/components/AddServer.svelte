ea29919d0c30ebfffcdfce1b15092c1ec871d0d7
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-12-22 19:50:56 -0800

    Add folder selection

--- src/lib/components/AddServer.svelte
+++ src/lib/components/AddServer.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
 	import { getVersionList } from '$lib/utils/versions';
+	import { open as openFolder } from '@tauri-apps/api/dialog';
 
 	export let open;
-	let versions = getVersionList();
+	let versions = getVersions();
+	let version: string;
+	let folder: string;
+
+	async function getVersions() {
+		const versionList = await getVersionList();
+		// get around svelte bug that I should go report
+		version = versionList[0];
+		return versionList;
+	}
+
+	const getFolder = async () => {
+		const path = (await openFolder({
+			directory: true,
+			recursive: true,
+		})) as string | null;
+
+		if (path) {
+			folder = path;
+		}
+	};
+
+	const onSubmit = () => {
+		console.log(version, folder);
+	};
 
 	const onKeyDown = (e: KeyboardEvent) => {
 		if (e.key === 'Escape') {
@@ -19,12 +44,12 @@
 >
 	<div class="flex w-1/2 flex-col items-center gap-4 rounded bg-neutral-600 p-6">
 		<h2 class="text-2xl">New Server</h2>
-		<form>
-			<div class="flex flex-col items-center justify-center gap-2">
+		<form on:submit|preventDefault={onSubmit}>
+			<div class="flex flex-col items-center justify-center gap-4">
 				<div>
-					<label for="type">Server type</label>
+					<label for="type">Type</label>
 					<select
-						class="mb-2 rounded border-2 border-neutral-200 bg-neutral-800 py-1"
+						class="rounded border-2 border-neutral-200 bg-neutral-800 py-1"
 						id="type"
 						title="Paper is the only supported option"
 						disabled
@@ -33,10 +58,14 @@
 					</select>
 				</div>
 				<div>
-					<label for="version">Server version</label>
-					<select class="mb-2 rounded border-2 border-neutral-200 bg-neutral-800 py-1" id="version">
+					<label for="version">Version</label>
+					<select
+						class="rounded border-2 border-neutral-200 bg-neutral-800 py-1"
+						id="version"
+						bind:value={version}
+					>
 						{#await versions}
-							<option value="1">Loading...</option>
+							<option>Loading...</option>
 						{:then versions}
 							{#each versions as version}
 								<option value={version}>{version}</option>
@@ -44,15 +73,32 @@
 						{/await}
 					</select>
 				</div>
+				<div>
+					<label for="folder" class="inline">Folder</label>
+					<input
+						disabled
+						type="text"
+						class="bg-neutral-800 text-sm text-neutral-500"
+						value={folder ?? 'No folder selected'}
+					/>
+					<input
+						on:click={getFolder}
+						id="folder"
+						type="button"
+						value="Select"
+						class="rounded border-2 border-neutral-200 bg-neutral-800 p-1 px-2 focus:border-blue-600"
+					/>
+				</div>
 				<div class="space-x-2">
 					<button
-						class="rounded border-neutral-300 bg-neutral-700 p-2 ring-2 ring-neutral-500 hover:bg-neutral-600 hover:ring-neutral-400"
+						on:click={() => (open = false)}
+						class="rounded bg-neutral-700 p-2 ring-2 ring-neutral-500 hover:bg-neutral-600 hover:ring-neutral-400"
 					>
 						Cancel
 					</button>
 					<button
 						type="submit"
-						class="rounded border-neutral-300 bg-blue-700 p-2 ring-2 ring-blue-600 hover:bg-blue-600 hover:ring-blue-500"
+						class="rounded bg-blue-700 p-2 ring-2 ring-blue-600 hover:bg-blue-600 hover:ring-blue-500"
 					>
 						Create
 					</button>
