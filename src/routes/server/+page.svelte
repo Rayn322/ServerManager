6be873ade227444f6cb4aff4b9fc04c59e77c706
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-12-24 18:20:57 -0800

    Make back button smaller

--- src/routes/server/+page.svelte
+++ src/routes/server/+page.svelte
@@ -11,6 +11,7 @@
 </script>
 
 <div class="fixed top-0 left-0 mt-14 flex h-screen w-64 flex-col bg-neutral-900 py-4">
+	<!-- TODO: make selected button a different color -->
 	<button class="p-2 hover:bg-neutral-700" on:click={() => (tab = 'console')}>Console</button>
 	<button class="p-2 hover:bg-neutral-700" on:click={() => (tab = 'settings')}>Settings</button>
 	<button class="p-2 hover:bg-neutral-700" on:click={() => (tab = 'details')}>Details</button>
@@ -35,7 +36,7 @@
 <div class="fixed bottom-8 left-8">
 	<button
 		on:click={() => goto('/')}
-		class="flex items-center justify-center rounded-xl bg-blue-800 p-4 text-xl hover:bg-blue-700"
+		class="flex items-center justify-center rounded-xl bg-blue-800 p-4 hover:bg-blue-700"
 	>
 		Back
 	</button>
