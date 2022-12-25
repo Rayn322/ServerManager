9951391231af67353a95f699344fe5c6833ca1f2
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-12-24 16:24:17 -0800

    Add navigation to and from server page

--- src/lib/components/ServerCard.svelte
+++ src/lib/components/ServerCard.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import type { Server } from '$lib/types/server';
+	import { openServerPage } from '$lib/utils/createServer';
 
 	export let server: Server;
 </script>
 
-<div class="flex h-24 w-48 items-center justify-center rounded-md bg-blue-900">
+<button
+	on:click={() => openServerPage(server.id)}
+	class="flex h-24 w-48 items-center justify-center rounded-md bg-blue-900 hover:bg-blue-800"
+>
 	<h1 class="text-xl">{server.name}</h1>
-</div>
+</button>
--- src/routes/server/+page.svelte
+++ src/routes/server/+page.svelte
@@ -4,6 +4,15 @@
 	const id = $page.url.searchParams.get('id');
 </script>
 
-<button on:click={() => goto('/')} class="rounded bg-blue-600 p-4">back</button>
 <p>This is server</p>
 <p>ID: {id}</p>
+
+<!-- <button on:click={() => goto('/')} class="rounded bg-blue-600 p-4">back</button> -->
+<div class="fixed bottom-8 left-8">
+	<button
+		on:click={() => goto('/')}
+		class="flex items-center justify-center rounded-xl bg-blue-800 p-4 text-xl hover:bg-blue-700"
+	>
+		Back
+	</button>
+</div>
