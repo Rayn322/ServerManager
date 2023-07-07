6bd8b0d759f2c3b3f309ba65956c3c12b57b4c56
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-07-07 16:38:56 -0700

    Remove explicit return type

--- src/lib/utils/versions.ts
+++ src/lib/utils/versions.ts
@@ -1,7 +1,7 @@
 import type { PaperVersionsList } from '$lib/types/paper';
 import { getClient } from '@tauri-apps/api/http';
 
-export async function getVersionList(): Promise<string[]> {
+export async function getVersionList() {
 	const client = await getClient();
 	const paperVersionsList = await (
 		await client.get<PaperVersionsList>('https://papermc.io/api/v2/projects/paper')
