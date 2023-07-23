3acdd9a35f1b7c49283b43f54870ad143f69777a
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-07-22 17:26:27 -0700

    Hide title and use overlay title bar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,17 @@
-<div class="h-14 border-b border-neutral-700 p-3">
-	<h1 class="text-2xl">MC Server Manager</h1>
+<script lang="ts">
+	import { type } from '@tauri-apps/api/os';
+
+	// temp fix until I can use transparent title bar and set background color
+	// TODO: see how this looks on windows and linux
+	async function isMacOS() {
+		return (await type()) === 'Darwin';
+	}
+</script>
+
+<div data-tauri-drag-region class="border-b border-neutral-700 p-3">
+	{#await isMacOS()}
+		<h1 data-tauri-drag-region class="text-2xl">MC Server Manager</h1>
+	{:then macOS}
+		<h1 data-tauri-drag-region class:mt-3={macOS} class="text-2xl">MC Server Manager</h1>
+	{/await}
 </div>
