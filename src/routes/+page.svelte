29de4ea483ce0b2077132c215d8100150edf1c21
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2025-12-22 19:31:15 -0800

    dialog stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { Plus } from "@lucide/svelte";
   import ServerCard from "../components/ServerCard.svelte";
+  import { open } from "@tauri-apps/plugin-dialog";
 
   let modalOpen = $state(true);
+  let path = $state("");
 </script>
 
 <!-- who cares about the layout for now -->
@@ -30,14 +32,68 @@
         if (e.currentTarget === e.target) modalOpen = false;
       }}
     >
-      <div class="flex flex-col gap-8 rounded-lg bg-neutral-900 p-8">
+      <div class="flex max-w-dvw flex-col gap-6 rounded-lg bg-neutral-900 p-8">
         <h2 class="text-2xl">Create New Server</h2>
-        <button
-          class="rounded bg-blue-500 px-4 py-2 text-white"
-          onclick={() => (modalOpen = false)}
-        >
-          Close
-        </button>
+        <div class="flex flex-col gap-4">
+          <div class="flex flex-col gap-2">
+            <label for="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              class="rounded bg-neutral-800 p-2 outline-none"
+              placeholder="My Cool Server"
+            />
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="version">Version</label>
+            <select
+              id="version"
+              class="rounded bg-neutral-800 p-2 outline-none"
+            >
+              <option>Placeholder</option>
+            </select>
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="path">Path</label>
+            <div class="flex flex-wrap gap-2">
+              <input
+                id="path"
+                type="text"
+                class="grow rounded bg-neutral-800 p-2 outline-none"
+                placeholder="/path/to/server"
+                bind:value={path}
+              />
+              <button
+                class="rounded bg-neutral-500 px-4 py-2 text-white"
+                onclick={async () => {
+                  const selectedPath = await open({
+                    directory: true,
+                    recursive: true,
+                  });
+                  if (selectedPath) {
+                    path = selectedPath;
+                  }
+                }}
+              >
+                Open
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex gap-2">
+          <button
+            class="cursor-pointer rounded bg-neutral-500 px-4 py-2 text-white"
+            onclick={() => (modalOpen = false)}
+          >
+            Close
+          </button>
+          <button
+            class="grow cursor-pointer rounded bg-green-700 px-4 py-2 text-white"
+          >
+            Create Server
+          </button>
+        </div>
       </div>
     </div>
   {/if}
