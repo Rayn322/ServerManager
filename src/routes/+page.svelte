a60902a69d0fb916e1d78876b8b36038d82637b1
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2025-12-22 19:00:55 -0800

    start dialog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { Plus } from "@lucide/svelte";
   import ServerCard from "../components/ServerCard.svelte";
+
+  let modalOpen = $state(true);
 </script>
 
 <!-- who cares about the layout for now -->
@@ -14,8 +16,29 @@
   <ServerCard name="Cool Server" />
   <ServerCard name="Cool Server" />
   <button
+    onclick={() => (modalOpen = true)}
     class="absolute right-8 bottom-8 flex cursor-pointer rounded-lg bg-green-700 p-4"
   >
     <Plus />
   </button>
+
+  {#if modalOpen}
+    <div
+      class="fixed inset-0 flex items-center justify-center bg-black/75"
+      role="presentation"
+      onmousedown={(e) => {
+        if (e.currentTarget === e.target) modalOpen = false;
+      }}
+    >
+      <div class="flex flex-col gap-8 rounded-lg bg-neutral-900 p-8">
+        <h2 class="text-2xl">Create New Server</h2>
+        <button
+          class="rounded bg-blue-500 px-4 py-2 text-white"
+          onclick={() => (modalOpen = false)}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  {/if}
 </main>
